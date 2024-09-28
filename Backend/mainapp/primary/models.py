0894f7149956767811a2f1c5375a3431ce8c542f
ceimos <86879499+ceimos@users.noreply.github.com>
2024-09-28 12:28:18 +0530

    gitignore

--- Backend/mainapp/primary/models.py
+++ Backend/mainapp/primary/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+class Cloth:
+    name = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=100)
+    usecount = models.IntegerField()
+    owner = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Outfit:
+    id = models.AutoField(primary_key=True)
+    usecount = models.IntegerField()
+    owner = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class OutfitCloth:
+    id = models.AutoField(primary_key=True)
+    outfit = models.ForeignKey('outfit', on_delete=models.CASCADE)
+    cloth = models.ForeignKey('Cloth', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Donation:
+    id = models.AutoField(primary_key=True)
+    doner = models.ForeignKey('User', on_delete=models.CASCADE)
+    reciever = models.ForeignKey('User', on_delete=models.CASCADE)
+    cloth = models.ForeignKey('Cloth', on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Sell:
+    id = models.AutoField(primary_key=True)
+    seller = models.ForeignKey('User', on_delete=models.CASCADE)
+    buyer = models.ForeignKey('User', on_delete=models.CASCADE)
+    cloth = models.ForeignKey('Cloth', on_delete=models.CASCADE)
+    date = models.DateField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    
+class SustainibilityDetails:
+    id = models.AutoField(primary_key=True)
+    cloth = models.ForeignKey('Cloth', on_delete=models.CASCADE)
+    carbonfootprint = models.FloatField()
+    waterfootprint = models.FloatField()
+    energyfootprint = models.FloatField()
+    wastefootprint = models.FloatField()
+
+    def __str__(self):
+        return self.name
